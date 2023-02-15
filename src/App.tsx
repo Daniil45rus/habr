d7be94cc3b8fa749782a151a8a563b425f6d516c
Daniil <cuharus2002@gmail.com>
2023-02-15 22:40:17 +0500

    Организация стилей. Добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Suspense } from 'react'
+import { Suspense, useContext, useState } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
+import { Theme, ThemeContext } from './theme/ThemeContext'
+import { useTheme } from './theme/useTheme'
+
 
 const App = () => {
+  const {theme, switchTheme} = useTheme()
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={() => switchTheme()}>Switch theme</button>
       <Link to={'/'}>
         Главня
       </Link>
